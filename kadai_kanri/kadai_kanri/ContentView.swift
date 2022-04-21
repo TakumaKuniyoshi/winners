a900be8bc21c6cd232954c133a64ec31b7685449
Keigo Unuma <keigo.u88@gmail.com>
2022-04-21 15:34:09 +0900

    add xcode project file

---
+++ kadai_kanri/kadai_kanri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kadai_kanri
+//
+//  Created by K.U on 2022/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kadai_kanri/kadai_kanri/kadai_kanriApp.swift
@@ -0,0 +1,17 @@
+//
+//  kadai_kanriApp.swift
+//  kadai_kanri
+//
+//  Created by K.U on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct kadai_kanriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
